82be9c2a900c4bf7f1fc9a2292ddf3ce6457500c
Werstan <88930205+Werstan@users.noreply.github.com>
2021-08-14 15:11:54 +0200

    update fix bug no alarm

--- client/main.lua
+++ client/main.lua
@@ -2,33 +2,14 @@ Citizen.CreateThread(function()
     while true do
         Wait(0)
         local ped = GetPlayerPed(-1)
-        if DoesEntityExist(GetVehiclePedIsTryingToEnter(GetVehicleDoorLockStatus(veh))) then
+        if DoesEntityExist(GetVehiclePedIsTryingToEnter(PlayerPedId(ped))) then
             local veh = GetVehiclePedIsTryingToEnter(PlayerPedId(ped))
             local lock = GetVehicleDoorLockStatus(veh)
             if lock == 7 then
                 SetVehicleDoorsLocked(veh, 7)
-                --SetVehicleAlarm(veh, true)
-                Wait(8)
-                SetVehicleAlarmTimeLeft(veh, 30000)
-            end            
-        end
-    end
-end)
-
-
-Citizen.CreateThread(function()
-    while true do
-        Wait(0)
-        local ped = GetPlayerPed(-1)
-        if DoesEntityExist(GetVehiclePedIsTryingToEnter(PlayerPedId(ped))) then
-            local veh = GetVehiclePedIsTryingToEnter(PlayerPedId(ped))
-            local npc = GetPedInVehicleSeat(veh, -1)
-            if npc then
-                SetVehicleDoorsLocked(veh, 7)
-                SetPedCanBeDraggedOut(npc, true)
-                --SetVehicleAlarm(veh, true)
-            end
-            
+                Wait(1000)
+                SetVehicleAlarmTimeLeft(veh, 20000)
+            end       
         end
     end
 end)
